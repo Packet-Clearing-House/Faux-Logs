7bc16875366ca7b43ca86049a38265334f43c827
Ths2-9Y-LqJt6 <mrjones@pch.net>
2018-01-16 14:58:56 -0800

    1.0 ready to go #1 dev-1

--- Faux-Logs-constants.php
+++ Faux-Logs-constants.php
@@ -1,6 +1,7 @@
 <?php
 
-define('FAUX_NL','\n');
-define('FAUX_IPv4','random_ipv4');
-define('FAUX_IPv6','random_ipv6');
-define('FAUX_DATE1','current_date');
+define('FAUX_NL','faux_new_line');
+define('FAUX_IPv4','faux_random_ipv4');
+define('FAUX_IPv6','faux_random_ipv6');
+define('FAUX_DATE1','faux_current_date');
+define('FAUX_ID_OFFSET', 5);
