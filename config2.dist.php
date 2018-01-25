7d1016d22ae1bd0211c9893f9cf0ea1bc076d5ec
Ths2-9Y-LqJt6 <mrjones@pch.net>
2018-01-25 14:04:57 -0800

    update readme, add config2.dist.php #12

---
+++ config2.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+$files = array();
+$files[] = 'foo.pch.net';
+$files[] = 'bar.pch.net';
+$files[] = 'bash.pch.net';
+$files[] = 'bang.pch.net';
+$files[] = 'qux.pch.net';
+
--- multi-file.gzip.php
+++ multi-file.gzip.php
@@ -15,15 +15,6 @@
 
 require_once("config2.php");
 
-if (!isset($files)) {
-    $files = array();
-    $files[] = 'foo.pch.net';
-    $files[] = 'bar.pch.net';
-    $files[] = 'bash.pch.net';
-    $files[] = 'bang.pch.net';
-    $files[] = 'qux.pch.net';
-}
-
 // init
 $sleep = 0;
 $startDate = time();
